57f69ec06b4a6eca8d9d3ae1eddd469d11cfae50
unknown <nisanthrtts@gmail.com>
2023-03-30 12:08:45 +0530

    minor fixes

---
+++ client/src/Redux/store.js
@@ -0,0 +1,27 @@
+import storage from "redux-persist/lib/storage"
+import { persistReducer, persistStore } from "redux-persist"
+import { configureStore, combineReducers } from "@reduxjs/toolkit"
+import thunk from "redux-thunk"
+import postReducer from "../features/post"
+import userReducer from "../features/user"
+import profileReducer from "../features/profileId"
+
+const persistConfig = {
+  key: "root",
+  storage,
+}
+
+const rootReducer = combineReducers({
+  post: postReducer,
+  user: userReducer,
+  profile: profileReducer,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: [thunk],
+})
+
+export const persistor = persistStore(store)
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,25 +4,17 @@ import App from "./App"
 import "./index.css"
 import { BrowserRouter } from "react-router-dom"
 import { Provider } from "react-redux"
-import { configureStore } from "@reduxjs/toolkit"
-import userReducer from "./features/user"
-import profileReducer from "./features/profileId"
-import postReducer from "./features/post"
-
-const store = configureStore({
-  reducer: {
-    user: userReducer,
-    profile: profileReducer,
-    post: postReducer,
-  },
-})
+import { persistor, store } from "./Redux/store"
+import { PersistGate } from "redux-persist/integration/react"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 )
